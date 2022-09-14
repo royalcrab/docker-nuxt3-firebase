14941a1fec2bcaa3b6dd0fcfbbf8b2ebeb53b50a
royalcrab <6356537+royalcrab@users.noreply.github.com>
2022-09-14 17:09:53 +0900

    nuxt error

--- files/nuxt-create.sh
+++ files/nuxt-create.sh
@@ -1,4 +1,8 @@
 nuxi init work
+if [ $? != 0 ]; then
+    echo 'Already exist work directory.'
+    exit 1
+fi
 cd work
 yarn add bootstrap @popperjs/core bootstrap-icons firebase
 cp -n /opt/firebase.json ./
